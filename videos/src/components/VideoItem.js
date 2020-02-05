040ae355ed3415c1191610b2b599dafed85b5f73
Jourdan Ooi <jungterooi@gmail.com>
2020-02-04 20:25:05 -0500

    finished videos app! On to Redux!

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,14 +1,14 @@
 import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({ video }) =>{
-return (
-    <div className ="video-item item">
-        <img clasName="ui image" src={video.snippet.thumbnails.medium.url} />
-        <div className = "content">
-            <div className="header">{video.snippet.title}</div>
+const VideoItem = ({ video, onVideoSelect }) => {
+    return (
+        <div onClick={() => onVideoSelect(video)} className="video-item item">
+            <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
         </div>
-    </div>
     );
 };
 
